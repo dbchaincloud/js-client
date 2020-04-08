c537cb6432853e0699a50f59237203ef099c5c53
Ethan Zhang <yzhang.wa@gmail.com>
2020-04-09 01:37:49 +0800

    to fix a minor bug

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ export * from './src/rest_client';
 export * from './src/key_manager';
 export * from './src/profile';
 export * from './src/friends';
-export { addExtraMsgConstructors } from './src/blockchain'
+export * from './src/blockchain'

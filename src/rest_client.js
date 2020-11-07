841f35075d90af1c065ab8da9e463d00a4e09446
xiaoyao <1134617484@qq.com>
2020-11-07 14:44:57 +0800

    Adds a method to get the specified table specified property

--- src/rest_client.js
+++ src/rest_client.js
@@ -105,10 +105,24 @@ async function getTableOptions(appCode, tableName) {
   return response.data.result;
 }
 
-async function getTable(appCode, tableName) {
+/**
+ * Query the attributes attached to the table
+ * @param {String} appCode this appCode
+ * @param {String} tableName The name of the table to query
+ * @param {String} name To query the value, can pass or not pass, do not pass or return all，options：['fields','filter','momes','name','owner','trigger']
+ * @returns Returns the corresponding property or all properties.  Object or Array
+ */
+async function getTableRaw(appCode, tableName, name='') {
   var uri = uriBuilder("tables", appCode, tableName);
   var response = await restGet(uri);
-  var fields = response.data.result.fields;
+  if(name){
+    return response.data.result[name];
+  }
+  return response.data.result;
+}
+
+async function getTable(appCode, tableName) {
+  var fields= await getTableRaw(appCode, tableName,'fields');
   var result = [];
   for (var i = 0; i < fields.length; i++) {
     var f = fields[i];

b7eaa11630e4ec5d689b8805042ef3077a4701a4
xiaoyao <1134617484@qq.com>
2020-12-02 12:30:42 +0800

    Add methods to verify that the chainId and access parameters are available

--- src/validate.js
+++ src/validate.js
@@ -1,4 +1,7 @@
 const CID = require('cids')
+import { setChainId, getChainId } from './blockchain';
+import { getBaseUrl, setBaseUrl } from './rest_lib';
+import { checkChainId} from "./rest_client"
 /**
  * Add a method to validate the 'CID' format, only the format, not the presence in the database
  * @param {String} cid 
@@ -14,4 +17,26 @@ function validateCID(cid) {
     return true;
 }
 
-export { validateCID };
+/**
+ * Determine if the chain address and chain ID are accessible
+ * @param {Sring} url The default is an existing URL, which can be passed in for the new link you want to access
+ * @param {String} chainId  The default is an existing chainId, passing in the chainId of the new chain you want to access
+ * @returns {boolean} Returns whether your address is accessible.  True or False
+ */
+async function detectChain(url = getBaseUrl(), chainId = getChainId()) {
+    let oldUrl = getBaseUrl();
+    let oldChainId = getChainId();
+    setBaseUrl(url)
+    setChainId(chainId)
+    try {
+        let isChainId =await checkChainId(chainId);
+        if(!isChainId)return {status:false,content:'当前chainId与访参不对应，请检查'};
+        return {status:true,content:''};
+    } catch (error) {
+        setBaseUrl(oldUrl)
+        setChainId(oldChainId)
+        return {status:false,content:'当前访参无法访问，请检查'};
+    }
+}
+
+export { validateCID, detectChain };

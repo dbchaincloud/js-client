e4dcd2373a5021416defcb543628e53fbd0a6c45
xiaolong <longjiuwei999@163.com>
2022-02-14 12:05:35 +0800

    use ethers generator bech32address and ethreum address and all files use ES6 modules

--- src/access_token.js
+++ src/access_token.js
@@ -1,14 +1,15 @@
 import { sign } from "./ethdbchain_sig/index"
 import { getPrivKey, getPubKey } from "./key_manager";
-const bs58 =  require("bs58")
+import bs58 from 'bs58'
+
 
 function signForToken(str) {
   var privKey = getPrivKey();
   var pubKey = getPubKey();
 
-  var sigObj = sign(str, privKey);
+  var signature = sign(str, privKey);
   var encodedPubKey = bs58.encode(pubKey)
-  var encodedSig = bs58.encode(sigObj.signature)
+  var encodedSig = bs58.encode(signature)
   var result = `${encodedPubKey}:${str}:${encodedSig}`
   return result;
 }

272dd7974b3fcb928e27a9a40fc7dcf030e9e7e3
xiaolong <longjiuwei999@163.com>
2022-02-24 14:39:55 +0800

    write test files

--- src/tx_factory/protoBuf/msgs_data.js
+++ src/tx_factory/protoBuf/msgs_data.js
@@ -1,16 +1,12 @@
-"use strict";
-exports.__esModule = true;
-exports.MsgFreezeRow = exports.MsgDeleteRow = exports.MsgUpdateRow = exports.MsgInsertRow = exports.protobufPackage = void 0;
 /* eslint-disable */
-var long_1 = require("long");
-var minimal_1 = require("protobufjs/minimal");
-exports.protobufPackage = "dbchain.msgs";
+import Long from "long";
+import _m0 from "protobufjs/minimal";
+export const protobufPackage = "dbchain.msgs";
 function createBaseMsgInsertRow() {
     return { owner: "", appCode: "", tableName: "", fields: new Uint8Array() };
 }
-exports.MsgInsertRow = {
-    encode: function (message, writer) {
-        if (writer === void 0) { writer = minimal_1["default"].Writer.create(); }
+export const MsgInsertRow = {
+    encode(message, writer = _m0.Writer.create()) {
         if (message.owner !== "") {
             writer.uint32(10).string(message.owner);
         }
@@ -25,12 +21,12 @@ exports.MsgInsertRow = {
         }
         return writer;
     },
-    decode: function (input, length) {
-        var reader = input instanceof minimal_1["default"].Reader ? input : new minimal_1["default"].Reader(input);
-        var end = length === undefined ? reader.len : reader.pos + length;
-        var message = createBaseMsgInsertRow();
+    decode(input, length) {
+        const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseMsgInsertRow();
         while (reader.pos < end) {
-            var tag = reader.uint32();
+            const tag = reader.uint32();
             switch (tag >>> 3) {
                 case 1:
                     message.owner = reader.string();
@@ -51,18 +47,18 @@ exports.MsgInsertRow = {
         }
         return message;
     },
-    fromJSON: function (object) {
+    fromJSON(object) {
         return {
             owner: isSet(object.owner) ? String(object.owner) : "",
             appCode: isSet(object.appCode) ? String(object.appCode) : "",
             tableName: isSet(object.tableName) ? String(object.tableName) : "",
             fields: isSet(object.fields)
                 ? bytesFromBase64(object.fields)
-                : new Uint8Array()
+                : new Uint8Array(),
         };
     },
-    toJSON: function (message) {
-        var obj = {};
+    toJSON(message) {
+        const obj = {};
         message.owner !== undefined && (obj.owner = message.owner);
         message.appCode !== undefined && (obj.appCode = message.appCode);
         message.tableName !== undefined && (obj.tableName = message.tableName);
@@ -70,15 +66,15 @@ exports.MsgInsertRow = {
             (obj.fields = base64FromBytes(message.fields !== undefined ? message.fields : new Uint8Array()));
         return obj;
     },
-    fromPartial: function (object) {
+    fromPartial(object) {
         var _a, _b, _c, _d;
-        var message = createBaseMsgInsertRow();
+        const message = createBaseMsgInsertRow();
         message.owner = (_a = object.owner) !== null && _a !== void 0 ? _a : "";
         message.appCode = (_b = object.appCode) !== null && _b !== void 0 ? _b : "";
         message.tableName = (_c = object.tableName) !== null && _c !== void 0 ? _c : "";
         message.fields = (_d = object.fields) !== null && _d !== void 0 ? _d : new Uint8Array();
         return message;
-    }
+    },
 };
 function createBaseMsgUpdateRow() {
     return {
@@ -86,12 +82,11 @@ function createBaseMsgUpdateRow() {
         appCode: "",
         tableName: "",
         id: 0,
-        fields: new Uint8Array()
+        fields: new Uint8Array(),
     };
 }
-exports.MsgUpdateRow = {
-    encode: function (message, writer) {
-        if (writer === void 0) { writer = minimal_1["default"].Writer.create(); }
+export const MsgUpdateRow = {
+    encode(message, writer = _m0.Writer.create()) {
         if (message.owner !== "") {
             writer.uint32(10).string(message.owner);
         }
@@ -109,12 +104,12 @@ exports.MsgUpdateRow = {
         }
         return writer;
     },
-    decode: function (input, length) {
-        var reader = input instanceof minimal_1["default"].Reader ? input : new minimal_1["default"].Reader(input);
-        var end = length === undefined ? reader.len : reader.pos + length;
-        var message = createBaseMsgUpdateRow();
+    decode(input, length) {
+        const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseMsgUpdateRow();
         while (reader.pos < end) {
-            var tag = reader.uint32();
+            const tag = reader.uint32();
             switch (tag >>> 3) {
                 case 1:
                     message.owner = reader.string();
@@ -138,7 +133,7 @@ exports.MsgUpdateRow = {
         }
         return message;
     },
-    fromJSON: function (object) {
+    fromJSON(object) {
         return {
             owner: isSet(object.owner) ? String(object.owner) : "",
             appCode: isSet(object.appCode) ? String(object.appCode) : "",
@@ -146,11 +141,11 @@ exports.MsgUpdateRow = {
             id: isSet(object.id) ? Number(object.id) : 0,
             fields: isSet(object.fields)
                 ? bytesFromBase64(object.fields)
-                : new Uint8Array()
+                : new Uint8Array(),
         };
     },
-    toJSON: function (message) {
-        var obj = {};
+    toJSON(message) {
+        const obj = {};
         message.owner !== undefined && (obj.owner = message.owner);
         message.appCode !== undefined && (obj.appCode = message.appCode);
         message.tableName !== undefined && (obj.tableName = message.tableName);
@@ -159,23 +154,22 @@ exports.MsgUpdateRow = {
             (obj.fields = base64FromBytes(message.fields !== undefined ? message.fields : new Uint8Array()));
         return obj;
     },
-    fromPartial: function (object) {
+    fromPartial(object) {
         var _a, _b, _c, _d, _e;
-        var message = createBaseMsgUpdateRow();
+        const message = createBaseMsgUpdateRow();
         message.owner = (_a = object.owner) !== null && _a !== void 0 ? _a : "";
         message.appCode = (_b = object.appCode) !== null && _b !== void 0 ? _b : "";
         message.tableName = (_c = object.tableName) !== null && _c !== void 0 ? _c : "";
         message.id = (_d = object.id) !== null && _d !== void 0 ? _d : 0;
         message.fields = (_e = object.fields) !== null && _e !== void 0 ? _e : new Uint8Array();
         return message;
-    }
+    },
 };
 function createBaseMsgDeleteRow() {
     return { owner: "", appCode: "", tableName: "", id: 0 };
 }
-exports.MsgDeleteRow = {
-    encode: function (message, writer) {
-        if (writer === void 0) { writer = minimal_1["default"].Writer.create(); }
+export const MsgDeleteRow = {
+    encode(message, writer = _m0.Writer.create()) {
         if (message.owner !== "") {
             writer.uint32(10).string(message.owner);
         }
@@ -190,12 +184,12 @@ exports.MsgDeleteRow = {
         }
         return writer;
     },
-    decode: function (input, length) {
-        var reader = input instanceof minimal_1["default"].Reader ? input : new minimal_1["default"].Reader(input);
-        var end = length === undefined ? reader.len : reader.pos + length;
-        var message = createBaseMsgDeleteRow();
+    decode(input, length) {
+        const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseMsgDeleteRow();
         while (reader.pos < end) {
-            var tag = reader.uint32();
+            const tag = reader.uint32();
             switch (tag >>> 3) {
                 case 1:
                     message.owner = reader.string();
@@ -216,38 +210,37 @@ exports.MsgDeleteRow = {
         }
         return message;
     },
-    fromJSON: function (object) {
+    fromJSON(object) {
         return {
             owner: isSet(object.owner) ? String(object.owner) : "",
             appCode: isSet(object.appCode) ? String(object.appCode) : "",
             tableName: isSet(object.tableName) ? String(object.tableName) : "",
-            id: isSet(object.id) ? Number(object.id) : 0
+            id: isSet(object.id) ? Number(object.id) : 0,
         };
     },
-    toJSON: function (message) {
-        var obj = {};
+    toJSON(message) {
+        const obj = {};
         message.owner !== undefined && (obj.owner = message.owner);
         message.appCode !== undefined && (obj.appCode = message.appCode);
         message.tableName !== undefined && (obj.tableName = message.tableName);
         message.id !== undefined && (obj.id = Math.round(message.id));
         return obj;
     },
-    fromPartial: function (object) {
+    fromPartial(object) {
         var _a, _b, _c, _d;
-        var message = createBaseMsgDeleteRow();
+        const message = createBaseMsgDeleteRow();
         message.owner = (_a = object.owner) !== null && _a !== void 0 ? _a : "";
         message.appCode = (_b = object.appCode) !== null && _b !== void 0 ? _b : "";
         message.tableName = (_c = object.tableName) !== null && _c !== void 0 ? _c : "";
         message.id = (_d = object.id) !== null && _d !== void 0 ? _d : 0;
         return message;
-    }
+    },
 };
 function createBaseMsgFreezeRow() {
     return { owner: "", appCode: "", tableName: "", id: 0 };
 }
-exports.MsgFreezeRow = {
-    encode: function (message, writer) {
-        if (writer === void 0) { writer = minimal_1["default"].Writer.create(); }
+export const MsgFreezeRow = {
+    encode(message, writer = _m0.Writer.create()) {
         if (message.owner !== "") {
             writer.uint32(10).string(message.owner);
         }
@@ -262,12 +255,12 @@ exports.MsgFreezeRow = {
         }
         return writer;
     },
-    decode: function (input, length) {
-        var reader = input instanceof minimal_1["default"].Reader ? input : new minimal_1["default"].Reader(input);
-        var end = length === undefined ? reader.len : reader.pos + length;
-        var message = createBaseMsgFreezeRow();
+    decode(input, length) {
+        const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseMsgFreezeRow();
         while (reader.pos < end) {
-            var tag = reader.uint32();
+            const tag = reader.uint32();
             switch (tag >>> 3) {
                 case 1:
                     message.owner = reader.string();
@@ -288,33 +281,33 @@ exports.MsgFreezeRow = {
         }
         return message;
     },
-    fromJSON: function (object) {
+    fromJSON(object) {
         return {
             owner: isSet(object.owner) ? String(object.owner) : "",
             appCode: isSet(object.appCode) ? String(object.appCode) : "",
             tableName: isSet(object.tableName) ? String(object.tableName) : "",
-            id: isSet(object.id) ? Number(object.id) : 0
+            id: isSet(object.id) ? Number(object.id) : 0,
         };
     },
-    toJSON: function (message) {
-        var obj = {};
+    toJSON(message) {
+        const obj = {};
         message.owner !== undefined && (obj.owner = message.owner);
         message.appCode !== undefined && (obj.appCode = message.appCode);
         message.tableName !== undefined && (obj.tableName = message.tableName);
         message.id !== undefined && (obj.id = Math.round(message.id));
         return obj;
     },
-    fromPartial: function (object) {
+    fromPartial(object) {
         var _a, _b, _c, _d;
-        var message = createBaseMsgFreezeRow();
+        const message = createBaseMsgFreezeRow();
         message.owner = (_a = object.owner) !== null && _a !== void 0 ? _a : "";
         message.appCode = (_b = object.appCode) !== null && _b !== void 0 ? _b : "";
         message.tableName = (_c = object.tableName) !== null && _c !== void 0 ? _c : "";
         message.id = (_d = object.id) !== null && _d !== void 0 ? _d : 0;
         return message;
-    }
+    },
 };
-var globalThis = (function () {
+var globalThis = (() => {
     if (typeof globalThis !== "undefined")
         return globalThis;
     if (typeof self !== "undefined")
@@ -325,29 +318,28 @@ var globalThis = (function () {
         return global;
     throw "Unable to locate global object";
 })();
-var atob = globalThis.atob ||
-    (function (b64) { return globalThis.Buffer.from(b64, "base64").toString("binary"); });
+const atob = globalThis.atob ||
+    ((b64) => globalThis.Buffer.from(b64, "base64").toString("binary"));
 function bytesFromBase64(b64) {
-    var bin = atob(b64);
-    var arr = new Uint8Array(bin.length);
-    for (var i = 0; i < bin.length; ++i) {
+    const bin = atob(b64);
+    const arr = new Uint8Array(bin.length);
+    for (let i = 0; i < bin.length; ++i) {
         arr[i] = bin.charCodeAt(i);
     }
     return arr;
 }
-var btoa = globalThis.btoa ||
-    (function (bin) { return globalThis.Buffer.from(bin, "binary").toString("base64"); });
+const btoa = globalThis.btoa ||
+    ((bin) => globalThis.Buffer.from(bin, "binary").toString("base64"));
 function base64FromBytes(arr) {
-    var bin = [];
-    for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-        var byte = arr_1[_i];
+    const bin = [];
+    for (const byte of arr) {
         bin.push(String.fromCharCode(byte));
     }
     return btoa(bin.join(""));
 }
-if (minimal_1["default"].util.Long !== long_1["default"]) {
-    minimal_1["default"].util.Long = long_1["default"];
-    minimal_1["default"].configure();
+if (_m0.util.Long !== Long) {
+    _m0.util.Long = Long;
+    _m0.configure();
 }
 function isSet(value) {
     return value !== null && value !== undefined;

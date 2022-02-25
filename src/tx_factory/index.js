7b7a75916d18791effe2239e7ca18c0dae25677a
xiaolong <longjiuwei999@163.com>
2022-02-25 17:52:59 +0800

    modify the prefix and coin type

--- src/tx_factory/index.js
+++ src/tx_factory/index.js
@@ -98,7 +98,7 @@ export default class Factory {
     const fee = {
       amount: [
         {
-          denom: "aphoton",
+          denom: "dbctoken",
           amount: String(parseInt(amount)),
         },
       ],

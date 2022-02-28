2a3b82ff28331073ced37d2074f6d492fb3f1740
xiaolong <longjiuwei999@163.com>
2022-02-28 17:48:52 +0800

    add protoBuf compile files

---
+++ src/tx_factory/protoBuf/coin.js
@@ -0,0 +1,205 @@
+/* eslint-disable */
+import Long from "long";
+import _m0 from "protobufjs/minimal";
+export const protobufPackage = "cosmos.base.v1beta1";
+function createBaseCoin() {
+    return { denom: "", amount: "" };
+}
+export const Coin = {
+    encode(message, writer = _m0.Writer.create()) {
+        if (message.denom !== "") {
+            writer.uint32(10).string(message.denom);
+        }
+        if (message.amount !== "") {
+            writer.uint32(18).string(message.amount);
+        }
+        return writer;
+    },
+    decode(input, length) {
+        const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseCoin();
+        while (reader.pos < end) {
+            const tag = reader.uint32();
+            switch (tag >>> 3) {
+                case 1:
+                    message.denom = reader.string();
+                    break;
+                case 2:
+                    message.amount = reader.string();
+                    break;
+                default:
+                    reader.skipType(tag & 7);
+                    break;
+            }
+        }
+        return message;
+    },
+    fromJSON(object) {
+        return {
+            denom: isSet(object.denom) ? String(object.denom) : "",
+            amount: isSet(object.amount) ? String(object.amount) : "",
+        };
+    },
+    toJSON(message) {
+        const obj = {};
+        message.denom !== undefined && (obj.denom = message.denom);
+        message.amount !== undefined && (obj.amount = message.amount);
+        return obj;
+    },
+    fromPartial(object) {
+        var _a, _b;
+        const message = createBaseCoin();
+        message.denom = (_a = object.denom) !== null && _a !== void 0 ? _a : "";
+        message.amount = (_b = object.amount) !== null && _b !== void 0 ? _b : "";
+        return message;
+    },
+};
+function createBaseDecCoin() {
+    return { denom: "", amount: "" };
+}
+export const DecCoin = {
+    encode(message, writer = _m0.Writer.create()) {
+        if (message.denom !== "") {
+            writer.uint32(10).string(message.denom);
+        }
+        if (message.amount !== "") {
+            writer.uint32(18).string(message.amount);
+        }
+        return writer;
+    },
+    decode(input, length) {
+        const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseDecCoin();
+        while (reader.pos < end) {
+            const tag = reader.uint32();
+            switch (tag >>> 3) {
+                case 1:
+                    message.denom = reader.string();
+                    break;
+                case 2:
+                    message.amount = reader.string();
+                    break;
+                default:
+                    reader.skipType(tag & 7);
+                    break;
+            }
+        }
+        return message;
+    },
+    fromJSON(object) {
+        return {
+            denom: isSet(object.denom) ? String(object.denom) : "",
+            amount: isSet(object.amount) ? String(object.amount) : "",
+        };
+    },
+    toJSON(message) {
+        const obj = {};
+        message.denom !== undefined && (obj.denom = message.denom);
+        message.amount !== undefined && (obj.amount = message.amount);
+        return obj;
+    },
+    fromPartial(object) {
+        var _a, _b;
+        const message = createBaseDecCoin();
+        message.denom = (_a = object.denom) !== null && _a !== void 0 ? _a : "";
+        message.amount = (_b = object.amount) !== null && _b !== void 0 ? _b : "";
+        return message;
+    },
+};
+function createBaseIntProto() {
+    return { int: "" };
+}
+export const IntProto = {
+    encode(message, writer = _m0.Writer.create()) {
+        if (message.int !== "") {
+            writer.uint32(10).string(message.int);
+        }
+        return writer;
+    },
+    decode(input, length) {
+        const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseIntProto();
+        while (reader.pos < end) {
+            const tag = reader.uint32();
+            switch (tag >>> 3) {
+                case 1:
+                    message.int = reader.string();
+                    break;
+                default:
+                    reader.skipType(tag & 7);
+                    break;
+            }
+        }
+        return message;
+    },
+    fromJSON(object) {
+        return {
+            int: isSet(object.int) ? String(object.int) : "",
+        };
+    },
+    toJSON(message) {
+        const obj = {};
+        message.int !== undefined && (obj.int = message.int);
+        return obj;
+    },
+    fromPartial(object) {
+        var _a;
+        const message = createBaseIntProto();
+        message.int = (_a = object.int) !== null && _a !== void 0 ? _a : "";
+        return message;
+    },
+};
+function createBaseDecProto() {
+    return { dec: "" };
+}
+export const DecProto = {
+    encode(message, writer = _m0.Writer.create()) {
+        if (message.dec !== "") {
+            writer.uint32(10).string(message.dec);
+        }
+        return writer;
+    },
+    decode(input, length) {
+        const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+        let end = length === undefined ? reader.len : reader.pos + length;
+        const message = createBaseDecProto();
+        while (reader.pos < end) {
+            const tag = reader.uint32();
+            switch (tag >>> 3) {
+                case 1:
+                    message.dec = reader.string();
+                    break;
+                default:
+                    reader.skipType(tag & 7);
+                    break;
+            }
+        }
+        return message;
+    },
+    fromJSON(object) {
+        return {
+            dec: isSet(object.dec) ? String(object.dec) : "",
+        };
+    },
+    toJSON(message) {
+        const obj = {};
+        message.dec !== undefined && (obj.dec = message.dec);
+        return obj;
+    },
+    fromPartial(object) {
+        var _a;
+        const message = createBaseDecProto();
+        message.dec = (_a = object.dec) !== null && _a !== void 0 ? _a : "";
+        return message;
+    },
+};
+if (_m0.util.Long !== Long) {
+    _m0.util.Long = Long;
+    _m0.configure();
+}
+function isSet(value) {
+    return value !== null && value !== undefined;
+}
